c641d3384c7b37869529e75c33df8126072766fa
wangxw <624020424@qq.com>
2017-04-08 13:14:48 +0800

    json数据处理

---
+++ app/src/main/java/android/easyweather/com/easyweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package android.easyweather.com.easyweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Lenovo on 2017/4/8.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address , okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
